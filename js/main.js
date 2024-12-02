599075681f32c0e5a46c1b5f03242f10cd178977
Adhityayusuf <adhitya.yusuf1206@gmail.com>
2024-12-02 17:47:12 +0700

    P

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('MAAFIN AKU YA').split('')
+    const titles = ('MAAFFIN AKU YA').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
