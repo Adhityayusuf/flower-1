b85ca01dc24a4a0521845c6fb1c301abd0eb0f13
Adhityayusuf <adhitya.yusuf1206@gmail.com>
2024-12-21 14:05:43 +0700

    p

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'HALO GYZNA LUZETTA YG CANTIK'.split('')
+const text = 'Assalamualaikum ibu ku yang cantik'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
